f1c2f1e0072659152d670ab0baf6f4951bf12c1b
xiehq <woodspirit@yeah.net>
2014-10-20 19:24:41 -0400

    change dir struct

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 
 @echo off
-FOR %%p in (*en.jemdoc) DO (@echo %%p to %%~np.html
-jemdoc.py -o en\ -c srs.conf "%%p")
-@echo 404.jemdoc to 404.html
-jemdoc.py -c srs.conf 404.jemdoc
+FOR %%p in (*.jemdoc) DO (@echo %%p to %%~np.html
+jemdoc.py -c srs.conf "%%p")
+@rem @echo 404.jemdoc to 404.html
+@rem jemdoc.py -c srs.conf 404.jemdoc
 @echo press ANY key ...
 @pause>NUL
 @echo off
