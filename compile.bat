72ac13a998a234706bd030c1f52c9fbb9610a95d
xiehq <woodspirit@yeah.net>
2014-10-20 19:16:56 -0400

    add 404 page

--- compile.bat
+++ compile.bat
@@ -2,6 +2,8 @@
 @echo off
 FOR %%p in (*en.jemdoc) DO (@echo %%p to %%~np.html
 jemdoc.py -o en\ -c srs.conf "%%p")
+@echo 404.jemdoc to 404.html
+jemdoc.py -c srs.conf 404.jemdoc
 @echo press ANY key ...
 @pause>NUL
 @echo off
